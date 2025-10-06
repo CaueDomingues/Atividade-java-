45f53abfa69d189bb0a9e34c2227f8c71af045df
Cauê Domingues Ferreira <cauedferreira@gmail.com>
2025-10-06 20:24:18 -0300

    Implementação do polimorfismo em Animal e criação da classe Peixe

--- Main.java
+++ Main.java
@@ -6,10 +6,20 @@ abstract class Animal {
         this.nome = nome;
     }
 
+    // Método abstrato (será sobrescrito nas subclasses)
     public abstract void emitirSom();
 
     public String getNome() {
         return nome;
+    }
+
+    // Polimorfismo por SOBRECARGA (mesmo nome, parâmetros diferentes)
+    public void comer() {
+        System.out.println(nome + " está comendo.");
+    }
+
+    public void comer(String comida) {
+        System.out.println(nome + " está comendo " + comida + ".");
     }
 }
 
@@ -19,17 +29,49 @@ class Cachorro extends Animal {
         super(nome);
     }
 
+    // Polimorfismo por SOBRESCRITA (reescreve o método da classe abstrata)
     @Override
     public void emitirSom() {
         System.out.println(nome + " está latindo: Au Au!");
+    }
+
+    // Sobrescrita também pode ser aplicada aqui:
+    @Override
+    public void comer() {
+        System.out.println(nome + " está comendo ração de cachorro.");
+    }
+}
+
+// Classe concreta Peixe
+class Peixe extends Animal {
+    public Peixe(String nome) {
+        super(nome);
+    }
+
+    @Override
+    public void emitirSom() {
+        System.out.println(nome + " não faz som perceptível.");
+    }
+
+    @Override
+    public void comer() {
+        System.out.println(nome + " está comendo algas.");
     }
 }
 
 // Classe pública Main (arquivo deve se chamar Main.java)
 public class Main {
     public static void main(String[] args) {
-        Cachorro meuCachorro = new Cachorro("Yumi"); // <-- aqui
-        System.out.println("Nome do meu animal de estimação: " + meuCachorro.getNome());
+        Cachorro meuCachorro = new Cachorro("Yumi");
+        Peixe meuPeixe = new Peixe("Nemo");
+
+        System.out.println("Nome do meu cachorro: " + meuCachorro.getNome());
         meuCachorro.emitirSom();
+        meuCachorro.comer();
+        meuCachorro.comer("biscoito");
+
+        System.out.println("\nNome do meu peixe: " + meuPeixe.getNome());
+        meuPeixe.emitirSom();
+        meuPeixe.comer();
     }
 }
